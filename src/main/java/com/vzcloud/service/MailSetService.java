1f999428827af327cc324cb9b57a7d5fd495611b
smsharm <sais@pruset.com>
2023-08-25 21:11:42 -0700

    first commit

---
+++ src/main/java/com/vzcloud/service/MailSetService.java
@@ -0,0 +1,54 @@
+package com.vzcloud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vzcloud.entity.MailSet;
+import com.vzcloud.mapper.MailSetMapper;
+import com.vzcloud.util.DateUtil;
+import com.vzcloud.util.UUIDUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @version v2.3
+ * @ClassName:DiskIoStateService.java
+ * @author: http://www.wgstart.com
+ * @date: November 16, 2019
+ * @Description: DiskIoStateService.java
+ * @Copyright: 2017-2022 wgcloud. All rights reserved.
+ */
+@Service
+public class MailSetService {
+
+
+    public void save(MailSet MailSet) throws Exception {
+        MailSet.setId(UUIDUtil.getUUID());
+        MailSet.setCreateTime(DateUtil.getNowTime());
+        MailSet.setFromMailName(MailSet.getFromMailName().trim());
+        MailSet.setFromPwd(MailSet.getFromPwd().trim());
+        MailSet.setToMail(MailSet.getToMail().trim());
+        MailSet.setSmtpHost(MailSet.getSmtpHost().trim());
+        mailSetMapper.save(MailSet);
+    }
+
+
+    public int deleteById(String[] id) throws Exception {
+        return mailSetMapper.deleteById(id);
+    }
+
+    public List<MailSet> selectAllByParams(Map<String, Object> params) throws Exception {
+        return mailSetMapper.selectAllByParams(params);
+    }
+
+    public int updateById(MailSet MailSet) throws Exception {
+        return mailSetMapper.updateById(MailSet);
+    }
+
+
+    @Autowired
+    private MailSetMapper mailSetMapper;
+
+
+}
